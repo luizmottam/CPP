020bdcd0cb35406dad038692a1767504ab00f43b
Luiz Motta <luizdamm@gmail.com>
2023-08-22 19:35:15 -0300

    Segunda leva de códigos

---
+++ do_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int cont = 20;
+
+    do {
+        cout << "Teste n " << cont << endl;
+        cont++;
+    } while(cont < 20);
+ 
+
+    return 0;
+}
